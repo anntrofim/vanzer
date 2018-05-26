766f804cbab02673e537d9f883005dcf9c5a0e4f
anntrofim <anita.trofim@gmail.com>
2018-05-26 15:59:14 +0300

    input.html - w3c validated; costom js in separate file

--- js/script-attempt.js
+++ js/script-attempt.js
@@ -1,31 +1,55 @@
+$(window).on("resize", function(event){
+	if($(window).width() < 768) {
+		$("#fullpage").fullpage.setAllowScrolling(false);
+		console.log($(window).width())
+	} else{
 
+	}
+});
 
 $(document).ready(function($) {
-	$("#fullpage").fullpage({
-		scrollingSpeed: 0,
-		verticalCentered: false,
-		navigation: true,
-		navigationPosition: 'left',
-		navigationTooltips: ['Home', 'About', 'Services', 'Portfolio', 'Blog', 'Contact'],
-		sectionsColor : ['#474747', '#fff', '#474747', '#474747', '#fff', '#474747'],
-	});
-	$('.autoplay').slick({
-		slidesToShow: 3,
-		slidesToScroll: 1,
-		autoplay: true,
-		autoplaySpeed: 300000000,
-		dots: true,
-		pauseOnHover: false,
-		arrows: false,
+
+
+	if($(window).width() > 768) {
+								// alert($(window).width())
+								$("#fullpage").fullpage({
+										// setAutoScrolling(true),
+										scrollingSpeed: 0,
+										verticalCentered: false,
+										navigation: true,
+										navigationPosition: 'left',
+										navigationTooltips: ['Home', 'About', 'Services', 'Portfolio', 'Blog', 'Contact'],
+										sectionsColor : ['#474747', '#fff', '#474747', '#474747', '#fff', '#474747'],
+
+
+									});
+							} else {
+								// alert($(window).width())
+								$("#fullpage").fullpage.setAllowScrolling(false);
+
+							}
+
+
+
+							$('.autoplay').slick({
+								slidesToShow: 3,
+								slidesToScroll: 1,
+								autoplay: true,
+								autoplaySpeed: 3000,
+								dots: true,
+								pauseOnHover: false,
+								arrows: false,
 				// centerMode: true,
 				// infinite: true,
 				// speed: 300,
 				// slidesToShow: 1,
 				// adaptiveHeight: true
 			});
-});
 
+
+						});
 function initMap() {
+
 	var vanzerOffice = {lat: 50.465920, lng: 30.514891};
 	var map = new google.maps.Map(document.getElementById('map'), {
 		zoom: 15,
@@ -126,5 +150,8 @@ var marker = new google.maps.Marker({
 	position: vanzerOffice,
 	map: map
 });
-}
 
+
+
+
+}
