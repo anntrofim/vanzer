93872977a565f105b724871854f05d7fd56f8646
Anna Trofimova <anita.trofim@gmail.com>
2018-07-27 22:25:04 +0300

    scss/ removed from .gitignore

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ $('.autoplay-p').slick({
 	slidesToShow: 3,
 	slidesToScroll: 1,
 	autoplay: true,
-	autoplaySpeed: 5000,
+	autoplaySpeed: 4000,
 	dots: true,
 	pauseOnHover: true,
 	arrows: false,
